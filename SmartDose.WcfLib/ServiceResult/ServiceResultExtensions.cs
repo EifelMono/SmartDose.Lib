86662ab9efe1ea510032a7d81a8f06fcefdcdf1a
Andreas Klapperich <Andreas@Klapperich.de>
2018-12-10 12:38:15 +0100

    ServiceResultExtensions

--- SmartDose.WcfLib/ServiceResult/ServiceResultExtensions.cs
+++ SmartDose.WcfLib/ServiceResult/ServiceResultExtensions.cs
@@ -8,5 +8,9 @@ namespace SmartDose.WcfLib
     {
         public static bool IsOk(this IServiceResult thisValue)
             => thisValue == null ? false : thisValue.Status == 0;
+        public static bool IsOkPlus(this IServiceResult thisValue)
+            => thisValue == null ? false : thisValue.Status >= 0;
+        public static bool IsError(this IServiceResult thisValue)
+            => thisValue == null ? false : thisValue.Status < 0;
     }
 }
